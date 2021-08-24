ea7eb7f3b23f9febb608c4d9bef4a095dae36cb0
Prodxgy <58959047+Prodxgy@users.noreply.github.com>
2021-08-24 15:01:17 -0400

    added card counting for deck

--- deck.py
+++ deck.py
@@ -5,11 +5,15 @@ class Deck():
     
     def __init__(self) -> None:
         self.drawn_cards = {'Hearts': [], 'Diamonds': [], 'Clubs': [], 'Spades': []}
-        self.drawn_count = 0
+        self.drawn = 0
+        self.remaining = 52
 
     def draw_card(self) -> card.Card:
         # TODO add a way to keep track of drawn cards
+        # update card counts
         self.drawn_count += 1
+        self.remaining -= 1
+        # draw card and append
         drawn_card = card.Card()
         self.drawn_cards[drawn_card.suit].append(drawn_card.value)
         return drawn_card
