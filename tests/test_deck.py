62c57b3db66e9cedc17490053f189c963242cf78
Prodxgy <58959047+Prodxgy@users.noreply.github.com>
2021-08-26 20:54:12 -0400

    updated test to reflect user inputted parameters

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,22 +1,24 @@
 from playingcards import Deck
 
 deck = Deck(seed="xyz")
-card = deck.draw_card()
+card_1 = deck.draw_card()
+card_2 = deck.draw_card(value=6, suit=2)
 
 """
 Test Deck Attributes
 """
 def test_drawn_cards():
-    assert deck.drawn_cards[2] == [6]
+    assert deck.drawn_cards[2] == [6, 6]
 
 def test_cards():
-    assert card is deck.cards[0]
+    assert card_1 is deck.cards[0]
+    assert card_2 is deck.cards[1]
 
 def test_drawn():
-    assert deck.drawn == 1
+    assert deck.drawn == 2
 
 def test_remaining():
-    assert deck.remaining == 51
+    assert deck.remaining == 50
 
 def test_shuffle():
     deck.shuffle()
@@ -29,22 +31,29 @@ def test_shuffle():
 Test Card Drawn from Deck
 """
 def test_value():
-    assert card.value == 6
+    assert card_1.value == 6
+    assert card_2.value == 6
 
 def test_rank():
-    assert card.rank == 6
+    assert card_1.rank == 6
+    assert card_2.rank == 6
 
 def test_suit():
-    assert card.suit == 2
+    assert card_1.suit == 2
+    assert card_2.suit == 2
 
 def test_suit_name():
-    assert card.suit_name == 'Hearts'
+    assert card_1.suit_name == 'Hearts'
+    assert card_2.suit_name == 'Hearts'
 
 def test_name():
-    assert card.name == '6 of Hearts'
+    assert card_1.name == '6 of Hearts'
+    assert card_2.name == '6 of Hearts'
 
 def test_img():
-    assert card.img == '*- - -*\n|♥    |\n|  6  |\n|   ♥ |\n*- - -*'
+    assert card_1.img == '*- - -*\n|♥    |\n|  6  |\n|   ♥ |\n*- - -*'
+    assert card_2.img == '*- - -*\n|♥    |\n|  6  |\n|   ♥ |\n*- - -*'
 
 def test_deck():
-    assert card.deck is deck
+    assert card_1.deck is deck
+    assert card_2.deck is deck
